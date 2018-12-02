beb5cd14309d0a49732a3bb860fe8108d0857866
frisk1995 <toshiki.priv@gmail.com>
2018-12-02 20:22:24 +0900

    add

--- output.php
+++ output.php
@@ -4,14 +4,39 @@
 <?php
   $name = $_POST['name'];
   $vocal = $_POST['vocal'];
-  $vocal = $_POST['gtvocal'];
+  $gtvocal = $_POST['gtvocal'];
   $lead = $_POST['lead'];
   $rhythm = $_POST['rhythm'];
   $drum = $_POST['drum'];
   $bass = $_POST['bass'];
+  $key = $_POST['key'];
+
 
   print ("次のデータを受け取りました<br />");
   print ("名前：$name<br />");
+  print ("パート<br />")
+  if(isset($vocal)){
+    print ("ボーカル<br />")
+  }
+  if(isset($gtvocal)){
+    print ("ギターボーカル<br />")
+  }
+  if(isset($lead)){
+    print ("リードギター<br />")
+  }
+  if(isset($rhythm)){
+    print ("リズムギター<br />")
+  }
+  if(isset($drum)){
+    print ("ドラム<br />")
+  }
+  if(isset($bass)){
+    print ("ベース<br />")
+  }
+  if(isset($key)){
+    print ("キーボード<br />")
+  }
+
 ?>
 </body>
 </html>

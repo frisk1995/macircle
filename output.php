ea47414161cd7d99b1610a93429b360bedeac300
frisk1995 <toshiki.priv@gmail.com>
2018-12-02 20:12:48 +0900

    add

--- output.php
+++ output.php
@@ -3,6 +3,13 @@
 <body>
 <?php
   $name = $_POST['name'];
+  $vocal = $_POST['vocal'];
+  $vocal = $_POST['gtvocal'];
+  $lead = $_POST['lead'];
+  $rhythm = $_POST['rhythm'];
+  $drum = $_POST['drum'];
+  $bass = $_POST['bass'];
+
   print ("次のデータを受け取りました<br />");
   print ("名前：$name<br />");
 ?>
